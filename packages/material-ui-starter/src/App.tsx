8b309ba9796f471dba9046bc78491bdbe71fac12
Jordan Sexton <jordan@jordansexton.com>
2021-07-30 14:54:06 -0500

    add material-ui-starter package

---
+++ packages/material-ui-starter/src/App.tsx
@@ -0,0 +1,46 @@
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { deepPurple } from '@material-ui/core/colors';
+import { SnackbarProvider } from 'notistack';
+import React, { FC } from 'react';
+import './App.css';
+import Wallet from './Wallet';
+
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: deepPurple[700],
+        },
+    },
+    overrides: {
+        MuiButtonBase: {
+            root: {
+                justifyContent: 'flex-start',
+            },
+        },
+        MuiButton: {
+            root: {
+                textTransform: undefined,
+                padding: '12px 16px',
+            },
+            startIcon: {
+                marginRight: 8,
+            },
+            endIcon: {
+                marginLeft: 8,
+            },
+        },
+    },
+});
+
+const App: FC = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <SnackbarProvider>
+                <Wallet />
+            </SnackbarProvider>
+        </ThemeProvider>
+    );
+};
+
+export default App;
---
+++ packages/material-ui-starter/src/Wallet.tsx
@@ -0,0 +1,52 @@
+import { WalletError } from '@solana/wallet-adapter-base';
+import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    getLedgerWallet,
+    getMathWallet,
+    getPhantomWallet,
+    getSolletWallet,
+    getSolongWallet,
+    getTorusWallet,
+} from '@solana/wallet-adapter-wallets';
+import { useSnackbar } from 'notistack';
+import React, { FC, useCallback, useMemo } from 'react';
+import Navigation from './Navigation';
+
+const Wallet: FC = () => {
+    // @solana/wallet-adapter-wallets imports all the adapters but supports tree shaking --
+    // Only the wallets you want to support will be compiled into your application
+    const wallets = useMemo(
+        () => [
+            getPhantomWallet(),
+            getLedgerWallet(),
+            getTorusWallet({
+                clientId: 'BOM5Cl7PXgE9Ylq1Z1tqzhpydY0RVr8k90QQ85N7AKI5QGSrr9iDC-3rvmy0K_hF0JfpLMiXoDhta68JwcxS1LQ',
+            }),
+            getSolongWallet(),
+            getMathWallet(),
+            // getWalletConnectWallet(), // @FIXME
+            getSolletWallet(),
+        ],
+        []
+    );
+
+    const { enqueueSnackbar } = useSnackbar();
+    const onError = useCallback(
+        (error: WalletError) => {
+            enqueueSnackbar(error.message ? `${error.name}: ${error.message}` : error.name, { variant: 'error' });
+            console.error(error);
+        },
+        [enqueueSnackbar]
+    );
+
+    return (
+        <WalletProvider wallets={wallets} onError={onError} autoConnect>
+            <WalletDialogProvider>
+                <Navigation />
+            </WalletDialogProvider>
+        </WalletProvider>
+    );
+};
+
+export default Wallet;
