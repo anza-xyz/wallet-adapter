e8b8e64aa48bf0ddbaf99a2e213159e7e03f05c8
Wang Ge <gw980531@gmail.com>
2021-08-23 19:08:53 +0800

    Added coin98 wallet.

--- packages/example/src/Wallet.tsx
+++ packages/example/src/Wallet.tsx
@@ -16,6 +16,7 @@ import {
 } from '@solana/wallet-adapter-ant-design';
 import { useLocalStorage, WalletProvider } from '@solana/wallet-adapter-react';
 import {
+    getCoin98Wallet,
     getLedgerWallet,
     getMathWallet,
     getPhantomWallet,
@@ -41,6 +42,7 @@ const Wallet: FC = () => {
                     clientId: 'BOM5Cl7PXgE9Ylq1Z1tqzhpydY0RVr8k90QQ85N7AKI5QGSrr9iDC-3rvmy0K_hF0JfpLMiXoDhta68JwcxS1LQ',
                 },
             }),
+            getCoin98Wallet(),
             getLedgerWallet(),
             getSolongWallet(),
             getMathWallet(),
--- packages/wallets/src/wallets.ts
+++ packages/wallets/src/wallets.ts
@@ -1,4 +1,5 @@
 import { WalletAdapter } from '@solana/wallet-adapter-base';
+import { Coin98WalletAdapter, Coin98WalletAdapterConfig } from '@solana/wallet-adapter-coin98';
 import { LedgerWalletAdapter, LedgerWalletAdapterConfig } from '@solana/wallet-adapter-ledger';
 import { MathWalletWalletAdapter, MathWalletWalletAdapterConfig } from '@solana/wallet-adapter-mathwallet';
 import { PhantomWalletAdapter, PhantomWalletAdapterConfig } from '@solana/wallet-adapter-phantom';
@@ -8,6 +9,7 @@ import { SolongWalletAdapter, SolongWalletAdapterConfig } from '@solana/wallet-a
 import { TorusWalletAdapter, TorusWalletAdapterConfig } from '@solana/wallet-adapter-torus';
 
 export enum WalletName {
+    Coin98 = 'Coin98',
     Ledger = 'Ledger',
     MathWallet = 'MathWallet',
     Phantom = 'Phantom',
@@ -26,6 +28,13 @@ export interface Wallet {
 }
 
 export const ICONS_URL = 'https://raw.githubusercontent.com/solana-labs/wallet-adapter/master/packages/wallets/icons';
+
+export const getBitpieWallet = (config?: Coin98WalletAdapterConfig): Wallet => ({
+    name: WalletName.Coin98,
+    url: 'https://coin98.com/',
+    icon: `${ICONS_URL}/coin98.svg`,
+    adapter: () => new Coin98WalletAdapter(config),
+});
 
 export const getLedgerWallet = (config?: LedgerWalletAdapterConfig): Wallet => ({
     name: WalletName.Ledger,
