367ca3fda699ce6466c2d19cd35c2acfdae71bf3
Jordan Sexton <jordan@jordansexton.com>
2021-08-25 16:10:08 -0500

    minor changes for memoization

--- packages/ant-design/src/WalletModal.tsx
+++ packages/ant-design/src/WalletModal.tsx
@@ -1,7 +1,7 @@
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletName } from '@solana/wallet-adapter-wallets';
 import { Menu, Modal, ModalProps } from 'antd';
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 import { useWalletModal } from './useWalletModal';
 import { WalletListItem } from './WalletListItem';
 
@@ -12,54 +12,65 @@ export interface WalletModalProps extends Omit<ModalProps, 'visible'> {
 export const WalletModal: FC<WalletModalProps> = ({
     title = 'Select your wallet',
     featuredWalletsNumber = 2,
+    onCancel,
     ...props
 }) => {
     const { wallets, select } = useWallet();
     const { visible, setVisible } = useWalletModal();
     const [expanded, setExpanded] = useState(false);
 
-    const featuredWallets = wallets.slice(0, featuredWalletsNumber);
-    const otherWallets = wallets.slice(featuredWalletsNumber);
+    const [featuredWallets, otherWallets, expands] = useMemo(
+        () => [
+            wallets.slice(0, featuredWalletsNumber),
+            wallets.slice(featuredWalletsNumber),
+            wallets.length > featuredWalletsNumber,
+        ],
+        [wallets, featuredWalletsNumber]
+    );
 
-    const showCollapse = featuredWalletsNumber < wallets.length;
+    const handleCancel = useCallback(
+        (event: React.MouseEvent<HTMLElement>) => {
+            if (onCancel) onCancel(event);
+            if (!event.defaultPrevented) setVisible(false);
+        },
+        [onCancel, setVisible]
+    );
 
-    const handleWalletClick = (walletName: WalletName) => {
-        select(walletName);
-        setVisible(false);
-    };
+    const handleWalletClick = useCallback(
+        (event: React.MouseEvent<HTMLElement>, walletName: WalletName) => {
+            select(walletName);
+            handleCancel(event);
+        },
+        [select, handleCancel]
+    );
 
-    const onCancel = useCallback(() => setVisible(false), [setVisible]);
-
-    const handleChange = () => {
-        setExpanded(!expanded);
-    };
+    const onOpenChange = useCallback(() => setExpanded(!expanded), [setExpanded, expanded]);
 
     return (
         <Modal
             title={title}
             visible={visible}
             centered={true}
-            onCancel={onCancel}
+            onCancel={handleCancel}
             footer={null}
             width={320}
             bodyStyle={{ padding: 0 }}
             {...props}
         >
-            <Menu className="wallet-adapter-modal-menu" inlineIndent={0} mode="inline" onOpenChange={handleChange}>
+            <Menu className="wallet-adapter-modal-menu" inlineIndent={0} mode="inline" onOpenChange={onOpenChange}>
                 {featuredWallets.map((wallet) => (
                     <WalletListItem
                         key={wallet.name}
-                        handleClick={() => handleWalletClick(wallet.name)}
+                        handleClick={(event) => handleWalletClick(event, wallet.name)}
                         wallet={wallet}
                     />
                 ))}
-
-                {showCollapse && (
+                {expands && (
                     <Menu.SubMenu key="wallet-adapter-modal-submenu" title={`${expanded ? 'Less' : 'More'} options`}>
                         {otherWallets.map((wallet) => (
                             <WalletListItem
                                 key={wallet.name}
-                                handleClick={() => handleWalletClick(wallet.name)}
+                                handleClick={(event) => handleWalletClick(event, wallet.name)}
                                 wallet={wallet}
                             />
                         ))}
