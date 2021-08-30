3a9839e40b036fd1742c6eecdd95be608f85b9f0
Jordan Sexton <jordan@jordansexton.com>
2021-08-30 18:29:33 -0500

    add message signing demo

--- packages/example/src/Demo.tsx
+++ packages/example/src/Demo.tsx
@@ -32,6 +32,7 @@ import React, { FC, useCallback, useMemo } from 'react';
 import { version } from '../package.json';
 import RequestAirdrop from './RequestAirdrop';
 import SendTransaction from './SendTransaction';
+import SignMessage from './SignMessage';
 
 export const Demo: FC = () => {
     const endpoint = useMemo(() => clusterApiUrl('devnet'), []);
@@ -73,9 +74,9 @@ export const Demo: FC = () => {
                         <Table>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell width={200}>Component</TableCell>
-                                    <TableCell width={200}>Material UI</TableCell>
-                                    <TableCell width={200}>Ant Design</TableCell>
+                                    <TableCell width={240}>Component</TableCell>
+                                    <TableCell width={240}>Material UI</TableCell>
+                                    <TableCell width={240}>Ant Design</TableCell>
                                     <TableCell>Example v{version}</TableCell>
                                 </TableRow>
                             </TableHead>
@@ -121,7 +122,6 @@ export const Demo: FC = () => {
                                     <TableCell></TableCell>
                                 </TableRow>
                                 <TableRow>
-                                    <TableCell></TableCell>
                                     <TableCell>
                                         <Tooltip title="Only runs if the wallet is ready to connect" placement="left">
                                             <FormControlLabel
@@ -142,6 +142,9 @@ export const Demo: FC = () => {
                                     </TableCell>
                                     <TableCell>
                                         <SendTransaction />
+                                    </TableCell>
+                                    <TableCell>
+                                        <SignMessage />
                                     </TableCell>
                                 </TableRow>
                             </TableBody>
