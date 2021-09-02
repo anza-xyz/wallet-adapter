0841287b616922f25aff86b8f2009fa049510998
Piotr Janosz <piotrjanosz@users.noreply.github.com>
2021-09-03 01:36:35 +0200

    Fixed width on modal (#47)

--- packages/react-ui/src/WalletModal.tsx
+++ packages/react-ui/src/WalletModal.tsx
@@ -119,64 +119,66 @@ export const WalletModal: FC<WalletModalProps> = ({
                 ref={ref}
                 role="dialog"
             >
-                <div className={`wallet-adapter-modal-wrapper ${!logo && 'wallet-adapter-modal-wrapper-no-logo'}`}>
-                    {logo && (
-                        <div className="wallet-adapter-modal-logo-wrapper">
-                            {typeof logo === 'string' ? (
-                                <img alt="logo" className="wallet-adapter-modal-logo" src={logo} />
-                            ) : (
-                                logo
-                            )}
-                        </div>
-                    )}
-                    <h1 className="wallet-adapter-modal-title" id="wallet-adapter-modal-title">
-                        Connect Wallet
-                    </h1>
-                    <button onClick={handleClose} className="wallet-adapter-modal-button-close">
-                        <svg width="14" height="14">
-                            <path d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z" />
-                        </svg>
-                    </button>
-                    <ul className="wallet-adapter-modal-list">
-                        {featured.map((wallet) => (
-                            <WalletListItem
-                                key={wallet.name}
-                                handleClick={(event) => handleWalletClick(event, wallet.name)}
-                                wallet={wallet}
-                            />
-                        ))}
-                    </ul>
-                    {more.length && (
-                        <>
-                            <Collapse expanded={expanded} id="wallet-adapter-modal-collapse">
-                                <ul className="wallet-adapter-modal-list">
-                                    {more.map((wallet) => (
-                                        <WalletListItem
-                                            key={wallet.name}
-                                            handleClick={(event) => handleWalletClick(event, wallet.name)}
-                                            tabIndex={expanded ? 0 : -1}
-                                            wallet={wallet}
-                                        />
-                                    ))}
-                                </ul>
-                            </Collapse>
-                            <Button
-                                aria-controls="wallet-adapter-modal-collapse"
-                                aria-expanded={expanded}
-                                className={`wallet-adapter-modal-collapse-button ${
-                                    expanded && 'wallet-adapter-modal-collapse-button-active'
-                                }`}
-                                endIcon={
-                                    <svg width="11" height="6" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z" />
-                                    </svg>
-                                }
-                                onClick={handleCollapseClick}
-                            >
-                                {expanded ? 'Less' : 'More'} options
-                            </Button>
-                        </>
-                    )}
+                <div className="wallet-adapter-modal-container">
+                    <div className={`wallet-adapter-modal-wrapper ${!logo && 'wallet-adapter-modal-wrapper-no-logo'}`}>
+                        {logo && (
+                            <div className="wallet-adapter-modal-logo-wrapper">
+                                {typeof logo === 'string' ? (
+                                    <img alt="logo" className="wallet-adapter-modal-logo" src={logo} />
+                                ) : (
+                                    logo
+                                )}
+                            </div>
+                        )}
+                        <h1 className="wallet-adapter-modal-title" id="wallet-adapter-modal-title">
+                            Connect Wallet
+                        </h1>
+                        <button onClick={handleClose} className="wallet-adapter-modal-button-close">
+                            <svg width="14" height="14">
+                                <path d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z" />
+                            </svg>
+                        </button>
+                        <ul className="wallet-adapter-modal-list">
+                            {featured.map((wallet) => (
+                                <WalletListItem
+                                    key={wallet.name}
+                                    handleClick={(event) => handleWalletClick(event, wallet.name)}
+                                    wallet={wallet}
+                                />
+                            ))}
+                        </ul>
+                        {more.length && (
+                            <>
+                                <Collapse expanded={expanded} id="wallet-adapter-modal-collapse">
+                                    <ul className="wallet-adapter-modal-list">
+                                        {more.map((wallet) => (
+                                            <WalletListItem
+                                                key={wallet.name}
+                                                handleClick={(event) => handleWalletClick(event, wallet.name)}
+                                                tabIndex={expanded ? 0 : -1}
+                                                wallet={wallet}
+                                            />
+                                        ))}
+                                    </ul>
+                                </Collapse>
+                                <Button
+                                    aria-controls="wallet-adapter-modal-collapse"
+                                    aria-expanded={expanded}
+                                    className={`wallet-adapter-modal-collapse-button ${
+                                        expanded && 'wallet-adapter-modal-collapse-button-active'
+                                    }`}
+                                    endIcon={
+                                        <svg width="11" height="6" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="m5.938 5.73 4.28-4.126a.915.915 0 0 0 0-1.322 1 1 0 0 0-1.371 0L5.253 3.736 1.659.272a1 1 0 0 0-1.371 0A.93.93 0 0 0 0 .932c0 .246.1.48.288.662l4.28 4.125a.99.99 0 0 0 1.37.01z" />
+                                        </svg>
+                                    }
+                                    onClick={handleCollapseClick}
+                                >
+                                    {expanded ? 'Less' : 'More'} options
+                                </Button>
+                            </>
+                        )}
+                    </div>
                 </div>
                 <div className="wallet-adapter-modal-overlay" onMouseDown={handleClose} />
             </div>,
