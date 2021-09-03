7ee6f7687bc0d479d4090ee7bf4fa53e7455b234
Jordan Sexton <jordan@jordansexton.com>
2021-09-03 18:27:32 -0500

    esbuild config

---
+++ packages/solflare/build.js
@@ -0,0 +1,11 @@
+const pkg = require('./package.json');
+
+require('esbuild').buildSync({
+    entryPoints: ['src/index.ts'],
+    outdir: 'lib',
+    bundle: true,
+    sourcemap: true,
+    preserveSymlinks: true,
+    target: 'es2021',
+    external: [...Object.keys(pkg.peerDependencies || {}), '*.css', '*.less'],
+});
