6bc6bb986628bf654ce957713be57a1a2a71bf2d
Jordan Sexton <jordan@jordansexton.com>
2021-09-10 16:45:20 -0500

    fix lint warning

--- packages/nextjs-starter/pages/_app.tsx
+++ packages/nextjs-starter/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { ReactElement } from 'react';
 import '../styles/globals.css';
 
 const WalletConnectionProvider = dynamic(() => import('../components/WalletConnectionProvider'), {
     ssr: false,
 });
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps): ReactElement {
     return (
         <WalletConnectionProvider>
             <Component {...pageProps} />
