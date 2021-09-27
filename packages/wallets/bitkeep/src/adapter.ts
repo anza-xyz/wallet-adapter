7c6698768dbd5e6a58a23be87dcfb5102c644c81
bitkeepwallet <85662627+bitkeepwallet@users.noreply.github.com>
2021-09-28 06:09:19 +0800

    Add BitKeep Wallet Support (#76)
    * Create tsconfig.json
    
    {
        "extends": "../../tsconfig.json",
        "compilerOptions": {
            "noEmit": false,
            "outDir": "lib"
        },
        "include": ["src"]
    }
    
    * Create package.json
    
    * Create README.md
    
    * Create LICENSE
    
    * Create index.ts
    
    * Create adapter.ts
    
    * Create .gitignore
    
    * Update wallets.ts
    
    * Add files via upload
    
    * Update README.md
    
    * Update adapter.ts
    
    * Update wallets.ts
    
    * fixes
    
    Co-authored-by: kevincomo <kevincomo@gmail.com>
    Co-authored-by: Jordan Sexton <jordan@jordansexton.com>

---
+++ packages/wallets/bitkeep/src/adapter.ts
@@ -0,0 +1,148 @@
+import {
+    BaseSignerWalletAdapter,
+    pollUntilReady,
+    WalletAccountError,
+    WalletDisconnectionError,
+    WalletNotConnectedError,
+    WalletNotFoundError,
+    WalletNotInstalledError,
+    WalletPublicKeyError,
+    WalletSignTransactionError,
+} from '@solana/wallet-adapter-base';
+import { PublicKey, Transaction } from '@solana/web3.js';
+
+interface BitKeepWallet {
+    isBitKeep?: boolean;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    getAccount(): Promise<string>;
+    signTransaction(transaction: Transaction): Promise<Transaction>;
+    signAllTransactions(transactions: Transaction[]): Promise<Transaction[]>;
+}
+
+interface BitKeepWindow extends Window {
+    bitkeep?: {
+        solana?: BitKeepWallet;
+    };
+}
+
+declare const window: BitKeepWindow;
+
+export interface BitKeepWalletAdapterConfig {
+    pollInterval?: number;
+    pollCount?: number;
+}
+
+export class BitKeepWalletAdapter extends BaseSignerWalletAdapter {
+    private _connecting: boolean;
+    private _wallet: BitKeepWallet | null;
+    private _publicKey: PublicKey | null;
+
+    constructor(config: BitKeepWalletAdapterConfig = {}) {
+        super();
+        this._connecting = false;
+        this._wallet = null;
+        this._publicKey = null;
+
+        if (!this.ready) pollUntilReady(this, config.pollInterval || 1000, config.pollCount || 3);
+    }
+
+    get publicKey(): PublicKey | null {
+        return this._publicKey;
+    }
+
+    get ready(): boolean {
+        return typeof window !== 'undefined' && !!window.bitkeep;
+    }
+
+    get connecting(): boolean {
+        return this._connecting;
+    }
+
+    get connected(): boolean {
+        return !!this._publicKey;
+    }
+
+    async connect(): Promise<void> {
+        try {
+            if (this.connected || this.connecting) return;
+            this._connecting = true;
+
+            const wallet = typeof window !== 'undefined' && window.bitkeep?.solana;
+            if (!wallet) throw new WalletNotFoundError();
+            if (!wallet.isBitKeep) throw new WalletNotInstalledError();
+
+            let account: string;
+            try {
+                account = await wallet.getAccount();
+            } catch (error: any) {
+                throw new WalletAccountError(error?.message, error);
+            }
+
+            let publicKey: PublicKey;
+            try {
+                publicKey = new PublicKey(account);
+            } catch (error: any) {
+                throw new WalletPublicKeyError(error?.message, error);
+            }
+
+            this._wallet = wallet;
+            this._publicKey = publicKey;
+
+            this.emit('connect');
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        } finally {
+            this._connecting = false;
+        }
+    }
+
+    async disconnect(): Promise<void> {
+        const wallet = this._wallet;
+        if (wallet) {
+            this._wallet = null;
+            this._publicKey = null;
+
+            try {
+                await wallet.disconnect();
+            } catch (error: any) {
+                this.emit('error', new WalletDisconnectionError(error?.message, error));
+            }
+
+            this.emit('disconnect');
+        }
+    }
+
+    async signTransaction(transaction: Transaction): Promise<Transaction> {
+        try {
+            const wallet = this._wallet;
+            if (!wallet) throw new WalletNotConnectedError();
+
+            try {
+                return (await wallet.signTransaction(transaction)) || transaction;
+            } catch (error: any) {
+                throw new WalletSignTransactionError(error?.message, error);
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
+    }
+
+    async signAllTransactions(transactions: Transaction[]): Promise<Transaction[]> {
+        try {
+            const wallet = this._wallet;
+            if (!wallet) throw new WalletNotConnectedError();
+
+            try {
+                return (await wallet.signAllTransactions(transactions)) || transactions;
+            } catch (error: any) {
+                throw new WalletSignTransactionError(error?.message, error);
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
+    }
+}
