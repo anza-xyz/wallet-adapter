c0d2e82fc46bc511a28439121d582c65518a7d74
Loris Leiva <loris.leiva@gmail.com>
2021-09-28 12:33:45 +0100

    Use @vue/reactivity to fix type errors/conflicts with react

--- packages/core/vue/src/useLocalStorage.ts
+++ packages/core/vue/src/useLocalStorage.ts
@@ -1,19 +1,19 @@
-import { customRef, Ref } from 'vue'
+import { customRef, Ref } from '@vue/reactivity';
 
 export default function<T> (key: string, defaultValue: T | null = null): Ref<T | null> {
     return customRef<T | null>((track, trigger) => ({
         get: () => {
-            track()
-            const value = localStorage.getItem(key)
-            return value ? JSON.parse(value) : defaultValue
+            track();
+            const value = localStorage.getItem(key);
+            return value ? JSON.parse(value) : defaultValue;
         },
         set: value => {
             if (value === null) {
-                localStorage.removeItem(key)
+                localStorage.removeItem(key);
             } else {
-                localStorage.setItem(key, JSON.stringify(value))
+                localStorage.setItem(key, JSON.stringify(value));
             }
-            trigger()
+            trigger();
         },
     }))
 }
