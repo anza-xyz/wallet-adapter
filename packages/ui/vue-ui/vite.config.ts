cdf823d00c462d079a96e0473dd1c0d51a1e2575
Jordan Sexton <jordan@jordansexton.com>
2021-10-14 15:14:12 -0500

    run yarn fmt

--- packages/ui/vue-ui/vite.config.ts
+++ packages/ui/vue-ui/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,4 +28,4 @@ export default defineConfig({
         },
     },
     plugins: [vue(), dts({ insertTypesEntry: true })],
-})
+});
