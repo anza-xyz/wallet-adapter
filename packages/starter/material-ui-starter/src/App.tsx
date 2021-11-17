7f0a767b701819ed083195673f2dbb5ad79cb2a4
Chris Chua <chris.sirhc@gmail.com>
2021-11-16 21:31:22 -0800

    clean up package deps

--- packages/starter/material-ui-starter/src/App.tsx
+++ packages/starter/material-ui-starter/src/App.tsx
@@ -3,14 +3,6 @@ import { deepPurple } from '@mui/material/colors';
 import { SnackbarProvider } from 'notistack';
 import React, { FC } from 'react';
 import { Wallet } from './Wallet';
-// TODO: Needed for the default theme override below. Remote later if it's not needed.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { createStyles } from '@mui/styles';
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
 
 // Use require instead of import, and order matters
 require('./App.css');
