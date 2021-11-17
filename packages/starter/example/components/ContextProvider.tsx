c632adf55b3e5c56af13cc149d8906e4971a57b3
Chris Chua <chris.sirhc@gmail.com>
2021-11-16 22:18:16 -0800

    remove unused mui/styles

--- packages/starter/example/components/ContextProvider.tsx
+++ packages/starter/example/components/ContextProvider.tsx
@@ -17,13 +17,6 @@ import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
 
 import { deepPurple, pink } from '@mui/material/colors';
 
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
-
 const theme = createTheme({
     palette: {
         mode: 'dark',
