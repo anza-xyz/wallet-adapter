0780c570c417842f725b76f8db3b14038caf0aa1
Alex Harley <alexbruceharley@gmail.com>
2021-12-05 14:54:57 +0100

    chore: reducing diff

--- packages/starter/react-ui-starter/src/Navigation.tsx
+++ packages/starter/react-ui-starter/src/Navigation.tsx
@@ -1,18 +1,16 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import React, { FC } from 'react';
 
 export const Navigation: FC = () => {
-    const { connection } = useConnection();
-
-    const { publicKey, sendTransaction } = useWallet();
+    const { wallet } = useWallet();
 
     return (
         <nav>
             <h1>Solana Starter App</h1>
             <div>
                 <WalletMultiButton />
-                {publicKey && <WalletDisconnectButton />}
+                {wallet && <WalletDisconnectButton />}
             </div>
         </nav>
     );
