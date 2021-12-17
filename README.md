389902ffa4ee41964b119013a55171222744b24b
Jordan Sexton <jordan@jordansexton.com>
2021-12-16 20:16:39 -0600

    starters: use getWallets

--- README.md
+++ README.md
@@ -47,15 +47,7 @@ yarn add @solana/wallet-adapter-base \
 import React, { FC, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-    getLedgerWallet,
-    getPhantomWallet,
-    getSlopeWallet,
-    getSolflareWallet,
-    getSolletExtensionWallet,
-    getSolletWallet,
-    getTorusWallet,
-} from '@solana/wallet-adapter-wallets';
+import { getWallets } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
@@ -73,17 +65,10 @@ export const Wallet: FC = () => {
     // You can also provide a custom RPC endpoint
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
-    // Only the wallets you configure here will be compiled into your application
-    const wallets = useMemo(() => [
-        getPhantomWallet(),
-        getSlopeWallet(),
-        getSolflareWallet(),
-        getTorusWallet(),
-        getLedgerWallet(),
-        getSolletWallet({ network }),
-        getSolletExtensionWallet({ network }),
-    ], [network]);
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded
+    const wallets = useMemo(() => getWallets({ network }), [network]);
 
     return (
         <ConnectionProvider endpoint={endpoint}>
--- packages/starter/nextjs-starter/components/WalletConnectionProvider.tsx
+++ packages/starter/nextjs-starter/components/WalletConnectionProvider.tsx
@@ -1,14 +1,6 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import {
-    getLedgerWallet,
-    getPhantomWallet,
-    getSlopeWallet,
-    getSolflareWallet,
-    getSolletExtensionWallet,
-    getSolletWallet,
-    getTorusWallet,
-} from '@solana/wallet-adapter-wallets';
+import { getWallets } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useMemo } from 'react';
 
@@ -19,20 +11,10 @@ export const WalletConnectionProvider: FC<{ children: ReactNode }> = ({ children
     // You can also provide a custom RPC endpoint
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
-    // Only the wallets you configure here will be compiled into your application
-    const wallets = useMemo(
-        () => [
-            getPhantomWallet(),
-            getSlopeWallet(),
-            getSolflareWallet(),
-            getTorusWallet(),
-            getLedgerWallet(),
-            getSolletWallet({ network }),
-            getSolletExtensionWallet({ network }),
-        ],
-        [network]
-    );
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded
+    const wallets = useMemo(() => getWallets({ network }), [network]);
 
     return (
         <ConnectionProvider endpoint={endpoint}>
