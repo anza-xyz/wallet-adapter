69d631bd1bd7d3ad68ea4f7fd5fc7199af741edf
Jordan Sexton <jordan@jordansexton.com>
2021-12-30 20:35:25 -0600

    Revert "don't use dynamic import for SSR anymore"
    This reverts commit 7c3466176c6542583ec4640a882207d5434b1870.

--- packages/starter/example/pages/_app.tsx
+++ packages/starter/example/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { FC } from 'react';
-import { ContextProvider } from '../components/ContextProvider';
+import { FC, ReactNode } from 'react';
 
 // Use require instead of import, and order matters
 require('antd/dist/antd.dark.less');
 require('@solana/wallet-adapter-ant-design/styles.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
+
+const ContextProvider = dynamic<{ children: ReactNode }>(
+    () => import('../components/ContextProvider').then(({ ContextProvider }) => ContextProvider),
+    {
+        ssr: false,
+    }
+);
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
     return (
