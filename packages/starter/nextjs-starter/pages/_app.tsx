0b7d7a2addfde938cb0319232702373dc77e91d8
Jordan Sexton <jordan@jordansexton.com>
2021-12-30 23:37:18 -0600

    Revert "don't use dynamic import for SSR anymore"
    This reverts commit 7c3466176c6542583ec4640a882207d5434b1870.

--- packages/starter/nextjs-starter/pages/_app.tsx
+++ packages/starter/nextjs-starter/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { AppProps } from 'next/app';
-import { FC } from 'react';
-import { WalletConnectionProvider } from '../components/WalletConnectionProvider';
+import dynamic from 'next/dynamic';
+import { FC, ReactNode } from 'react';
 
 // Use require instead of import, and order matters
 require('../styles/globals.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
+
+const WalletConnectionProvider = dynamic<{ children: ReactNode }>(
+    () =>
+        import('../components/WalletConnectionProvider').then(
+            ({ WalletConnectionProvider }) => WalletConnectionProvider
+        ),
+    {
+        ssr: false,
+    }
+);
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
     return (
