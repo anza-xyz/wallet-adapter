8fbefd8dbe49a05c39533e64243514cff633ca93
Jordan Sexton <jordan@jordansexton.com>
2022-01-04 18:18:41 -0600

    style fixes

--- packages/ui/react-ui/src/WalletModal.tsx
+++ packages/ui/react-ui/src/WalletModal.tsx
@@ -183,10 +183,7 @@ export const WalletModal: FC<WalletModalProps> = ({ className = '', container = 
                                                 expanded ? 'wallet-adapter-modal-list-more-icon-rotate' : ''
                                             }`}
                                         >
-                                            <path
-                                                d="M0.71418 1.626L5.83323 6.26188C5.91574 6.33657 6.0181 6.39652 6.13327 6.43762C6.24844 6.47872 6.37371 6.5 6.50048 6.5C6.62725 6.5 6.75252 6.47872 6.8677 6.43762C6.98287 6.39652 7.08523 6.33657 7.16774 6.26188L12.2868 1.626C12.7753 1.1835 12.3703 0.5 11.6195 0.5H1.37997C0.629216 0.5 0.224175 1.1835 0.71418 1.626Z"
-                                                fill="wallet-adapter-modal-button-icon-fill"
-                                            />
+                                            <path d="M0.71418 1.626L5.83323 6.26188C5.91574 6.33657 6.0181 6.39652 6.13327 6.43762C6.24844 6.47872 6.37371 6.5 6.50048 6.5C6.62725 6.5 6.75252 6.47872 6.8677 6.43762C6.98287 6.39652 7.08523 6.33657 7.16774 6.26188L12.2868 1.626C12.7753 1.1835 12.3703 0.5 11.6195 0.5H1.37997C0.629216 0.5 0.224175 1.1835 0.71418 1.626Z" />
                                         </svg>
                                     </button>
                                 ) : null}
@@ -230,7 +227,7 @@ export const WalletModal: FC<WalletModalProps> = ({ className = '', container = 
                                             </button>
                                         </h2>
                                         <Collapse expanded={expanded} id="wallet-adapter-modal-collapse">
-                                            <ul className="wallet-adapter-modal-list wallet-modal-mt-24">
+                                            <ul className="wallet-adapter-modal-list">
                                                 {otherWallets.map((wallet) => (
                                                     <WalletListItem
                                                         key={wallet.adapter.name}
