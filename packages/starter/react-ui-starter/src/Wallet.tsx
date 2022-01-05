0ce4a338fe5447e0fe82b3ac53f5d1de8c2056b9
Jordan Sexton <jordan@jordansexton.com>
2022-01-04 20:14:30 -0600

    make wallet order consistent

--- packages/starter/react-ui-starter/src/Wallet.tsx
+++ packages/starter/react-ui-starter/src/Wallet.tsx
@@ -29,9 +29,9 @@ export const Wallet: FC = () => {
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
-            new TorusWalletAdapter(),
             new SlopeWalletAdapter(),
             new SolflareWalletAdapter(),
+            new TorusWalletAdapter(),
             new LedgerWalletAdapter(),
             new SolletWalletAdapter({ network }),
             new SolletExtensionWalletAdapter({ network }),
