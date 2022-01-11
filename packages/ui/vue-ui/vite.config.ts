44af7dac2be68206437f1ec32dbdcf9d4c9168b4
Jordan Sexton <jordan@jordansexton.com>
2022-01-11 11:41:50 -0600

    have vite target es2019

--- packages/ui/vue-ui/vite.config.ts
+++ packages/ui/vue-ui/vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        target: 'esnext',
+        target: 'es2019',
         outDir: 'lib/esm',
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
