9a14722361fa64dc2d2df9f44bdb3ca30f09b71f
ProPratik1405 <sariapratik@gmail.com>
2022-02-15 16:09:58 +0545

    Small Comment

--- packages/starter/react-ui-starter/src/index.tsx
+++ packages/starter/react-ui-starter/src/index.tsx
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+// A Comment to create a pull request
+
 ReactDOM.render(
     <StrictMode>
         <App />
