8a563779535825a3907912e2037d62b237b0e25d
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-03-02 14:17:35 +0000

    Warn on the console when the WalletModalProvider is missing

--- packages/ui/react-ui/src/useWalletModal.tsx
+++ packages/ui/react-ui/src/useWalletModal.tsx
@@ -5,7 +5,30 @@ export interface WalletModalContextState {
     setVisible: (open: boolean) => void;
 }
 
-export const WalletModalContext = createContext<WalletModalContextState>({} as WalletModalContextState);
+const DEFAULT_CONTEXT = {
+    setVisible(_open: boolean) {
+        console.error(
+            'You have tried to call `setVisible` on a `WalletModalContext` ' +
+                'without providing one. Make sure to render a ' +
+                '`<WalletModalProvider>` as an ancestor of the component ' +
+                'that uses `WalletModalContext`.' 
+        );
+    },
+    visible: false,
+};
+Object.defineProperty(DEFAULT_CONTEXT, 'visible', {
+    get() {
+        console.error(
+            'You have tried to read `visible` on a `WalletModalContext` ' + 
+                'without providing one. Make sure to render a ' + 
+                '`<WalletModalProvider>` as an ancestor of the component ' +
+                'that uses `WalletModalContext`.'
+        );
+        return false;
+    }
+})
+
+export const WalletModalContext = createContext<WalletModalContextState>(DEFAULT_CONTEXT as WalletModalContextState);
 
 export function useWalletModal(): WalletModalContextState {
     return useContext(WalletModalContext);
