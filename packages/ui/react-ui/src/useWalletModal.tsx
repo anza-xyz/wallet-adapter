f7ffb5fd525944c2a3f11996762ae0e1d3769435
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-03-02 18:22:32 +0000

    update modal contexts with helper functions

--- packages/ui/react-ui/src/useWalletModal.tsx
+++ packages/ui/react-ui/src/useWalletModal.tsx
@@ -1,32 +1,12 @@
 import { createContext, useContext } from 'react';
+import { defaultValueForModalProvider } from '@solana/wallet-adapter-react';
 
 export interface WalletModalContextState {
     visible: boolean;
     setVisible: (open: boolean) => void;
 }
 
-const DEFAULT_CONTEXT = {
-    setVisible(_open: boolean) {
-        console.error(
-            'You have tried to call `setVisible` on a `WalletModalContext` ' +
-                'without providing one. Make sure to render a ' +
-                '`<WalletModalProvider>` as an ancestor of the component ' +
-                'that uses `WalletModalContext`.' 
-        );
-    },
-    visible: false,
-};
-Object.defineProperty(DEFAULT_CONTEXT, 'visible', {
-    get() {
-        console.error(
-            'You have tried to read `visible` on a `WalletModalContext` ' + 
-                'without providing one. Make sure to render a ' + 
-                '`<WalletModalProvider>` as an ancestor of the component ' +
-                'that uses `WalletModalContext`.'
-        );
-        return false;
-    }
-})
+const DEFAULT_CONTEXT = defaultValueForModalProvider('setVisible', 'visible', 'WalletModalContext', 'WalletModalProvider');
 
 export const WalletModalContext = createContext<WalletModalContextState>(DEFAULT_CONTEXT as WalletModalContextState);
 
