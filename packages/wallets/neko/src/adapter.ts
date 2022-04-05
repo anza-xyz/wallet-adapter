672427620c9dfb10658ed91bc67ba1ad2fdb2261
minhdc <minhdc@fabbi.io>
2022-04-05 14:47:24 +0700

    fixed prettier

--- packages/wallets/neko/src/adapter.ts
+++ packages/wallets/neko/src/adapter.ts
@@ -1,46 +1,46 @@
 import {
-  BaseMessageSignerWalletAdapter,
-  EventEmitter,
-  scopePollingDetectionStrategy,
-  SendTransactionOptions,
-  WalletAccountError,
-  WalletConnectionError,
-  WalletDisconnectedError,
-  WalletDisconnectionError,
-  WalletError,
-  WalletName,
-  WalletNotConnectedError,
-  WalletNotReadyError,
-  WalletPublicKeyError,
-  WalletReadyState,
-  WalletSignTransactionError,
-  WalletWindowClosedError,
+    BaseMessageSignerWalletAdapter,
+    EventEmitter,
+    scopePollingDetectionStrategy,
+    SendTransactionOptions,
+    WalletAccountError,
+    WalletConnectionError,
+    WalletDisconnectedError,
+    WalletDisconnectionError,
+    WalletError,
+    WalletName,
+    WalletNotConnectedError,
+    WalletNotReadyError,
+    WalletPublicKeyError,
+    WalletReadyState,
+    WalletSignTransactionError,
+    WalletWindowClosedError,
 } from '@solana/wallet-adapter-base';
 import { Connection, PublicKey, SendOptions, Transaction, TransactionSignature } from '@solana/web3.js';
 
 interface NekoWalletEvents {
-  connect(...args: unknown[]): unknown;
-  disconnect(...args: unknown[]): unknown;
+    connect(...args: unknown[]): unknown;
+    disconnect(...args: unknown[]): unknown;
 }
 
 interface NekoWallet extends EventEmitter<NekoWalletEvents> {
-  isNeko?: boolean;
-  publicKey?: { toBytes(): Uint8Array };
-  isConnected: boolean;
-  signTransaction(transaction: Transaction): Promise<Transaction>;
-  signAllTransactions(transactions: Transaction[]): Promise<Transaction[]>;
-  signAndSendTransaction(
-      transaction: Transaction,
-      options?: SendOptions
-  ): Promise<{ signature: TransactionSignature }>;
-  signMessage(message: Uint8Array): Promise<{ signature: Uint8Array }>;
-  connect(): Promise<void>;
-  disconnect(): Promise<void>;
-  _handleDisconnect(...args: unknown[]): unknown;
+    isNeko?: boolean;
+    publicKey?: { toBytes(): Uint8Array };
+    isConnected: boolean;
+    signTransaction(transaction: Transaction): Promise<Transaction>;
+    signAllTransactions(transactions: Transaction[]): Promise<Transaction[]>;
+    signAndSendTransaction(
+        transaction: Transaction,
+        options?: SendOptions
+    ): Promise<{ signature: TransactionSignature }>;
+    signMessage(message: Uint8Array): Promise<{ signature: Uint8Array }>;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    _handleDisconnect(...args: unknown[]): unknown;
 }
 
 interface NekoWalletWindow extends Window {
-  solana?: NekoWallet;
+    solana?: NekoWallet;
 }
 
 declare const window: NekoWalletWindow;
@@ -50,218 +50,218 @@ export interface NekoWalletAdapterConfig {}
 export const NekoWalletName = 'Neko (Mobile)' as WalletName<'Neko (Mobile)'>;
 
 export class NekoWalletAdapter extends BaseMessageSignerWalletAdapter {
-  name = NekoWalletName;
-  url = 'https://nekowallet.com/';
-  icon = 'https://cdn.discordapp.com/attachments/953573404395585596/960393815208914964/Group_7996_1.png';
+    name = NekoWalletName;
+    url = 'https://nekowallet.com/';
+    icon = 'https://cdn.discordapp.com/attachments/953573404395585596/960393815208914964/Group_7996_1.png';
 
-  private _connecting: boolean;
-  private _wallet: NekoWallet | null;
-  private _publicKey: PublicKey | null;
-  private _readyState: WalletReadyState =
-      typeof window === 'undefined' || typeof document === 'undefined'
-          ? WalletReadyState.Unsupported
-          : WalletReadyState.NotDetected;
+    private _connecting: boolean;
+    private _wallet: NekoWallet | null;
+    private _publicKey: PublicKey | null;
+    private _readyState: WalletReadyState =
+        typeof window === 'undefined' || typeof document === 'undefined'
+            ? WalletReadyState.Unsupported
+            : WalletReadyState.NotDetected;
 
-  constructor(config: NekoWalletAdapterConfig = {}) {
-      super();
-      this._connecting = false;
-      this._wallet = null;
-      this._publicKey = null;
+    constructor(config: NekoWalletAdapterConfig = {}) {
+        super();
+        this._connecting = false;
+        this._wallet = null;
+        this._publicKey = null;
 
-      if (this._readyState !== WalletReadyState.Unsupported) {
-          scopePollingDetectionStrategy(() => {
-              if (window.solana?.isNeko) {
-                  this._readyState = WalletReadyState.Installed;
-                  this.emit('readyStateChange', this._readyState);
-                  return true;
-              }
-              return false;
-          });
-      }
-  }
+        if (this._readyState !== WalletReadyState.Unsupported) {
+            scopePollingDetectionStrategy(() => {
+                if (window.solana?.isNeko) {
+                    this._readyState = WalletReadyState.Installed;
+                    this.emit('readyStateChange', this._readyState);
+                    return true;
+                }
+                return false;
+            });
+        }
+    }
 
-  get publicKey(): PublicKey | null {
-      return this._publicKey;
-  }
+    get publicKey(): PublicKey | null {
+        return this._publicKey;
+    }
 
-  get connecting(): boolean {
-      return this._connecting;
-  }
+    get connecting(): boolean {
+        return this._connecting;
+    }
 
-  get connected(): boolean {
-      return !!this._wallet?.isConnected;
-  }
+    get connected(): boolean {
+        return !!this._wallet?.isConnected;
+    }
 
-  get readyState(): WalletReadyState {
-      return this._readyState;
-  }
+    get readyState(): WalletReadyState {
+        return this._readyState;
+    }
 
-  async connect(): Promise<void> {
-      try {
-          if (this.connected || this.connecting) return;
-          if (this._readyState !== WalletReadyState.Installed) throw new WalletNotReadyError();
+    async connect(): Promise<void> {
+        try {
+            if (this.connected || this.connecting) return;
+            if (this._readyState !== WalletReadyState.Installed) throw new WalletNotReadyError();
 
-          this._connecting = true;
+            this._connecting = true;
 
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const wallet = window!.solana!;
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const wallet = window!.solana!;
 
-          if (!wallet.isConnected) {
-              // HACK: Neko doesn't reject or emit an event if the popup is closed
-              const handleDisconnect = wallet._handleDisconnect;
-              try {
-                  await new Promise<void>((resolve, reject) => {
-                      const connect = () => {
-                          wallet.off('connect', connect);
-                          resolve();
-                      };
+            if (!wallet.isConnected) {
+                // HACK: Neko doesn't reject or emit an event if the popup is closed
+                const handleDisconnect = wallet._handleDisconnect;
+                try {
+                    await new Promise<void>((resolve, reject) => {
+                        const connect = () => {
+                            wallet.off('connect', connect);
+                            resolve();
+                        };
 
-                      wallet._handleDisconnect = (...args: unknown[]) => {
-                          wallet.off('connect', connect);
-                          reject(new WalletWindowClosedError());
-                          return handleDisconnect.apply(wallet, args);
-                      };
+                        wallet._handleDisconnect = (...args: unknown[]) => {
+                            wallet.off('connect', connect);
+                            reject(new WalletWindowClosedError());
+                            return handleDisconnect.apply(wallet, args);
+                        };
 
-                      wallet.on('connect', connect);
+                        wallet.on('connect', connect);
 
-                      wallet.connect().catch((reason: any) => {
-                          wallet.off('connect', connect);
-                          reject(reason);
-                      });
-                  });
-              } catch (error: any) {
-                  if (error instanceof WalletError) throw error;
-                  throw new WalletConnectionError(error?.message, error);
-              } finally {
-                  wallet._handleDisconnect = handleDisconnect;
-              }
-          }
+                        wallet.connect().catch((reason: any) => {
+                            wallet.off('connect', connect);
+                            reject(reason);
+                        });
+                    });
+                } catch (error: any) {
+                    if (error instanceof WalletError) throw error;
+                    throw new WalletConnectionError(error?.message, error);
+                } finally {
+                    wallet._handleDisconnect = handleDisconnect;
+                }
+            }
 
-          if (!wallet.publicKey) throw new WalletAccountError();
+            if (!wallet.publicKey) throw new WalletAccountError();
 
-          let publicKey: PublicKey;
-          try {
-              publicKey = new PublicKey(wallet.publicKey.toBytes());
-          } catch (error: any) {
-              throw new WalletPublicKeyError(error?.message, error);
-          }
+            let publicKey: PublicKey;
+            try {
+                publicKey = new PublicKey(wallet.publicKey.toBytes());
+            } catch (error: any) {
+                throw new WalletPublicKeyError(error?.message, error);
+            }
 
-          wallet.on('disconnect', this._disconnected);
+            wallet.on('disconnect', this._disconnected);
 
-          this._wallet = wallet;
-          this._publicKey = publicKey;
+            this._wallet = wallet;
+            this._publicKey = publicKey;
 
-          this.emit('connect', publicKey);
-      } catch (error: any) {
-          this.emit('error', error);
-          throw error;
-      } finally {
-          this._connecting = false;
-      }
-  }
+            this.emit('connect', publicKey);
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        } finally {
+            this._connecting = false;
+        }
+    }
 
-  async disconnect(): Promise<void> {
-      const wallet = this._wallet;
-      if (wallet) {
-          wallet.off('disconnect', this._disconnected);
+    async disconnect(): Promise<void> {
+        const wallet = this._wallet;
+        if (wallet) {
+            wallet.off('disconnect', this._disconnected);
 
-          this._wallet = null;
-          this._publicKey = null;
+            this._wallet = null;
+            this._publicKey = null;
 
-          try {
-              await wallet.disconnect();
-          } catch (error: any) {
-              this.emit('error', new WalletDisconnectionError(error?.message, error));
-          }
-      }
+            try {
+                await wallet.disconnect();
+            } catch (error: any) {
+                this.emit('error', new WalletDisconnectionError(error?.message, error));
+            }
+        }
 
-      this.emit('disconnect');
-  }
+        this.emit('disconnect');
+    }
 
-  async sendTransaction(
-      transaction: Transaction,
-      connection: Connection,
-      options?: SendTransactionOptions
-  ): Promise<TransactionSignature> {
-      try {
-          const wallet = this._wallet;
-          // Neko doesn't handle partial signers, so if they are provided, don't use `signAndSendTransaction`
-          if (wallet && 'signAndSendTransaction' in wallet && !options?.signers) {
-              // HACK: Neko's `signAndSendTransaction` should always set these, but doesn't yet
-              transaction.feePayer = transaction.feePayer || this.publicKey || undefined;
-              transaction.recentBlockhash =
-                  transaction.recentBlockhash || (await connection.getRecentBlockhash('finalized')).blockhash;
+    async sendTransaction(
+        transaction: Transaction,
+        connection: Connection,
+        options?: SendTransactionOptions
+    ): Promise<TransactionSignature> {
+        try {
+            const wallet = this._wallet;
+            // Neko doesn't handle partial signers, so if they are provided, don't use `signAndSendTransaction`
+            if (wallet && 'signAndSendTransaction' in wallet && !options?.signers) {
+                // HACK: Neko's `signAndSendTransaction` should always set these, but doesn't yet
+                transaction.feePayer = transaction.feePayer || this.publicKey || undefined;
+                transaction.recentBlockhash =
+                    transaction.recentBlockhash || (await connection.getRecentBlockhash('finalized')).blockhash;
 
-              const { signature } = await wallet.signAndSendTransaction(transaction, options);
-              return signature;
-          }
-      } catch (error: any) {
-          this.emit('error', error);
-          throw error;
-      }
+                const { signature } = await wallet.signAndSendTransaction(transaction, options);
+                return signature;
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
 
-      return await super.sendTransaction(transaction, connection, options);
-  }
+        return await super.sendTransaction(transaction, connection, options);
+    }
 
-  async signTransaction(transaction: Transaction): Promise<Transaction> {
-      try {
-          const wallet = this._wallet;
-          if (!wallet) throw new WalletNotConnectedError();
+    async signTransaction(transaction: Transaction): Promise<Transaction> {
+        try {
+            const wallet = this._wallet;
+            if (!wallet) throw new WalletNotConnectedError();
 
-          try {
-              return (await wallet.signTransaction(transaction)) || transaction;
-          } catch (error: any) {
-              throw new WalletSignTransactionError(error?.message, error);
-          }
-      } catch (error: any) {
-          this.emit('error', error);
-          throw error;
-      }
-  }
+            try {
+                return (await wallet.signTransaction(transaction)) || transaction;
+            } catch (error: any) {
+                throw new WalletSignTransactionError(error?.message, error);
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
+    }
 
-  async signAllTransactions(transactions: Transaction[]): Promise<Transaction[]> {
-      try {
-          const wallet = this._wallet;
-          if (!wallet) throw new WalletNotConnectedError();
+    async signAllTransactions(transactions: Transaction[]): Promise<Transaction[]> {
+        try {
+            const wallet = this._wallet;
+            if (!wallet) throw new WalletNotConnectedError();
 
-          try {
-              return (await wallet.signAllTransactions(transactions)) || transactions;
-          } catch (error: any) {
-              throw new WalletSignTransactionError(error?.message, error);
-          }
-      } catch (error: any) {
-          this.emit('error', error);
-          throw error;
-      }
-  }
+            try {
+                return (await wallet.signAllTransactions(transactions)) || transactions;
+            } catch (error: any) {
+                throw new WalletSignTransactionError(error?.message, error);
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
+    }
 
-  async signMessage(message: Uint8Array): Promise<Uint8Array> {
-      try {
-          const wallet = this._wallet;
-          if (!wallet) throw new WalletNotConnectedError();
+    async signMessage(message: Uint8Array): Promise<Uint8Array> {
+        try {
+            const wallet = this._wallet;
+            if (!wallet) throw new WalletNotConnectedError();
 
-          try {
-              const { signature } = await wallet.signMessage(message);
-              return signature;
-          } catch (error: any) {
-              throw new WalletSignTransactionError(error?.message, error);
-          }
-      } catch (error: any) {
-          this.emit('error', error);
-          throw error;
-      }
-  }
+            try {
+                const { signature } = await wallet.signMessage(message);
+                return signature;
+            } catch (error: any) {
+                throw new WalletSignTransactionError(error?.message, error);
+            }
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        }
+    }
 
-  private _disconnected = () => {
-      const wallet = this._wallet;
-      if (wallet) {
-          wallet.off('disconnect', this._disconnected);
+    private _disconnected = () => {
+        const wallet = this._wallet;
+        if (wallet) {
+            wallet.off('disconnect', this._disconnected);
 
-          this._wallet = null;
-          this._publicKey = null;
+            this._wallet = null;
+            this._publicKey = null;
 
-          this.emit('error', new WalletDisconnectedError());
-          this.emit('disconnect');
-      }
-  };
+            this.emit('error', new WalletDisconnectedError());
+            this.emit('disconnect');
+        }
+    };
 }
