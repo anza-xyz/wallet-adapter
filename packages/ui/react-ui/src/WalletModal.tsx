d1f4515a0b18cb46dd3212a287143f49745ca844
Jason Safaiyeh <safaiyeh@protonmail.com>
2022-04-06 11:40:14 -0700

    Lint

--- packages/ui/react-ui/src/WalletModal.tsx
+++ packages/ui/react-ui/src/WalletModal.tsx
@@ -42,10 +42,11 @@ export const WalletModal: FC<WalletModalProps> = ({ className = '', container = 
     const getStartedWallet = useMemo(() => {
         return installedWallets.length
             ? installedWallets[0]
-            : defaultGetStartedWallet ?? (wallets.find((wallet: { adapter: { name: WalletName } }) => wallet.adapter.name === 'Torus') ||
-                  wallets.find((wallet: { adapter: { name: WalletName } }) => wallet.adapter.name === 'Phantom') ||
-                  wallets.find((wallet: { readyState: any }) => wallet.readyState === WalletReadyState.Loadable) ||
-                  otherWallets[0]);
+            : defaultGetStartedWallet ??
+                  (wallets.find((wallet: { adapter: { name: WalletName } }) => wallet.adapter.name === 'Torus') ||
+                      wallets.find((wallet: { adapter: { name: WalletName } }) => wallet.adapter.name === 'Phantom') ||
+                      wallets.find((wallet: { readyState: any }) => wallet.readyState === WalletReadyState.Loadable) ||
+                      otherWallets[0]);
     }, [installedWallets, wallets, otherWallets]);
 
     const hideModal = useCallback(() => {
