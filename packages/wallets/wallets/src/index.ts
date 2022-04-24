abe732b98138fcf47743aa25f94da26b7fce1ce4
mrzhao <zhaomengru2018@gmail.com>
2022-04-24 16:32:32 +0800

    Add Keystone Wallet adapter

--- packages/wallets/wallets/src/index.ts
+++ packages/wallets/wallets/src/index.ts
@@ -27,3 +27,4 @@ export * from '@solana/wallet-adapter-solong';
 export * from '@solana/wallet-adapter-spot';
 export * from '@solana/wallet-adapter-tokenpocket';
 export * from '@solana/wallet-adapter-torus';
+export * from '@solana/wallet-adapter-keystone';
