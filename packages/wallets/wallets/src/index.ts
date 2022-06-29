40a6bb7e305e6efe19e026bb557d4f9312f05496
skywalletadmin <admin@getsky.app>
2022-06-29 09:48:38 +0530

    Removed lib files from PR

--- packages/wallets/wallets/src/index.ts
+++ packages/wallets/wallets/src/index.ts
@@ -25,5 +25,4 @@ export * from '@solana/wallet-adapter-solong';
 export * from '@solana/wallet-adapter-spot';
 export * from '@solana/wallet-adapter-tokenpocket';
 export * from '@solana/wallet-adapter-torus';
-export * from '@solana/wallet-adapter-walletconnect';
 
