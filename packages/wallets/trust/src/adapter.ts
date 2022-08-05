c0f7c5787a405f20f5e54d0159d020da5264771e
Jordan Sexton <jordan@jordansexton.com>
2022-08-04 19:43:12 -0500

    add preflight commitment

--- packages/wallets/trust/src/adapter.ts
+++ packages/wallets/trust/src/adapter.ts
@@ -173,6 +173,8 @@ export class TrustWalletAdapter extends BaseMessageSignerWalletAdapter {
                 const { signers, ...sendOptions } = options;
                 signers?.length && transaction.partialSign(...signers);
 
+                sendOptions.preflightCommitment = sendOptions.preflightCommitment || connection.commitment;
+
                 const { signature } = await wallet.signAndSendTransaction(transaction, sendOptions);
                 return signature;
             } catch (error: any) {
