57d9ca5599ee20164da68ff99e8a386a2cb475dc
John Rees <johnrees@users.noreply.github.com>
2022-08-28 18:35:25 -0400

    eslint rule to enforce .js on relative import paths
    https://github.com/solana-labs/wallet-adapter/issues/546

---
+++ .eslint/eslint-plugin-relative-files/index.js
@@ -0,0 +1,32 @@
+const { existsSync } = require('fs');
+const { dirname, join } = require('path');
+
+module.exports.rules = {
+    'must-end-with-js': {
+        meta: {
+            fixable: true,
+        },
+        create: function (context) {
+            const fixSourcePath = (node) => {
+                const source = node.source.value;
+                // if it's a relative import or export that doesn't end with .js
+                if (source.startsWith('.') && !source.endsWith('.js')) {
+                    const fullPath = join(dirname(context.getFilename()), node.source.value);
+                    // and a .tsx? file exists
+                    if (existsSync(`${fullPath}.ts`) || existsSync(`${fullPath}.tsx`)) {
+                        context.report({
+                            node,
+                            message: 'Relative imports and exports must end with .js',
+                            fix: (fixer) => fixer.replaceText(node.source, `'${node.source.value}.js'`),
+                        });
+                    }
+                }
+            };
+
+            return {
+                ExportAllDeclaration: fixSourcePath,
+                ImportDeclaration: fixSourcePath,
+            };
+        },
+    },
+};
