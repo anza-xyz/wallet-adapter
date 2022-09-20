6546bad079f184a879e4fdd55e2f39716522a28c
Jordan Sexton <jordan@jordansexton.com>
2022-09-20 18:18:01 -0500

    burner -> unsafe-burner

--- packages/wallets/unsafe-burner/src/adapter.ts
+++ packages/wallets/unsafe-burner/src/adapter.ts
@@ -3,14 +3,14 @@ import { BaseSignerWalletAdapter, WalletNotConnectedError, WalletReadyState } fr
 import type { PublicKey, Transaction, TransactionVersion, VersionedTransaction } from '@solana/web3.js';
 import { Keypair } from '@solana/web3.js';
 
-export const BurnerWalletName = 'Burner Wallet' as WalletName<'Burner Wallet'>;
+export const UnsafeBurnerWalletName = 'Burner Wallet' as WalletName<'Burner Wallet'>;
 
 /**
  * This burner wallet adapter is unsafe to use and is only included to provide an easy way for applications to test
  * Wallet Adapter without using a third-party wallet.
  */
 export class UnsafeBurnerWalletAdapter extends BaseSignerWalletAdapter {
-    name = BurnerWalletName;
+    name = UnsafeBurnerWalletName;
     url = 'https://github.com/solana-labs/wallet-adapter#usage';
     icon =
         'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzQiIGhlaWdodD0iMzAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0zNCAxMC42djIuN2wtOS41IDE2LjVoLTQuNmw2LTEwLjVhMi4xIDIuMSAwIDEgMCAyLTMuNGw0LjgtOC4zYTQgNCAwIDAgMSAxLjMgM1ptLTQuMyAxOS4xaC0uNmw0LjktOC40djQuMmMwIDIuMy0yIDQuMy00LjMgNC4zWm0yLTI4LjRjLS4zLS44LTEtMS4zLTItMS4zaC0xLjlsLTIuNCA0LjNIMzBsMS43LTNabS0zIDVoLTQuNkwxMC42IDI5LjhoNC43TDI4LjggNi40Wk0xOC43IDBoNC42bC0yLjUgNC4zaC00LjZMMTguNiAwWk0xNSA2LjRoNC42TDYgMjkuOEg0LjJjLS44IDAtMS43LS4zLTIuNC0uOEwxNSA2LjRaTTE0IDBIOS40TDcgNC4zaDQuNkwxNCAwWm0tMy42IDYuNEg1LjdMMCAxNi4ydjhMMTAuMyA2LjRaTTQuMyAwaC40TDAgOC4ydi00QzAgMiAxLjkgMCA0LjMgMFoiIGZpbGw9IiM5OTQ1RkYiLz48L3N2Zz4=';
