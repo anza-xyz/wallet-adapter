8949881175d3aa28efb62c287e3bcc667e1e8e7b
Steven Luscher <steveluscher@users.noreply.github.com>
2022-09-20 17:38:55 -0700

    Repair tests and run in CI (#571)
    * fix: repair tests so that `pnpm test` does a clean run
    
    * feat: run tests for changed packages on all pull requests

--- packages/core/react/jest.config.js
+++ packages/core/react/jest.config.js
@@ -1,12 +1,21 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-    preset: 'ts-jest/presets/js-with-ts-esm',
-    testEnvironment: 'node',
+    preset: 'ts-jest/presets/default-esm',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    resolver: `${__dirname}/jest.resolver.cjs`,
     globals: {
         IS_REACT_ACT_ENVIRONMENT: true,
         'ts-jest': {
-            tsconfig: './tsconfig.test.json',
+            tsconfig: './tsconfig.tests.json',
         },
     },
-    transformIgnorePatterns: ['/node_modules\\/uuid\\//'],
+    testEnvironment: 'node',
+    transformIgnorePatterns: ['/node_modules/(?!(uuid))'],
 };
