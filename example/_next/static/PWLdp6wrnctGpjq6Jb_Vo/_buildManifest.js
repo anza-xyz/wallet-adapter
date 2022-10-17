e8800285f295b8acf1cf0cd869a765ad9de5f391
Jordan Sexton <jordan@jordansexton.com>
2022-10-17 11:23:08 -0500

    Updates

---
+++ example/_next/static/PWLdp6wrnctGpjq6Jb_Vo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/991-5723d2c970e7afe9.js","static/chunks/pages/index-f79a56be0862eedc.js"],"/_error":["static/chunks/pages/_error-fad245cbda7a1ddd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
