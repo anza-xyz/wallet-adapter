f8a2bca97fb324a570334fadafab1658c8561cb5
Jordan Sexton <jordan@jordansexton.com>
2022-10-26 16:01:43 -0500

    update readme

--- packages/starter/nextjs-starter/src/pages/_app.tsx
+++ packages/starter/nextjs-starter/src/pages/_app.tsx
@@ -23,14 +23,14 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
             /**
              * Wallets that implement either of these standards will be available automatically.
              *
-             *   - Solana Mobile Stack Mobile Wallet Adatper Protocol
+             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
              *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-             *   - Wallet Standard
-             *     (https://github.com/wallet-standard/wallet-standard)
+             *   - Solana Wallet Standard
+             *     (https://github.com/solana-labs/wallet-standard)
              *
-             * If you wish to support a wallet that supports none of thost standards, instantiate
-             * its legacy wallet adapter here. Common legacy adapters can be found in the npm
-             * package `@solana/wallet-adapter-wallets`.
+             * If you wish to support a wallet that supports neither of those standards,
+             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+             * in the npm package `@solana/wallet-adapter-wallets`.
              */
             new UnsafeBurnerWalletAdapter(),
         ],
