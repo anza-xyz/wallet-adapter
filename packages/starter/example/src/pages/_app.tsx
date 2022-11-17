230ad1c834f101ec0f59ea51fda1017321fd122e
Jordan Sexton <jordan@jordansexton.com>
2022-11-17 14:14:54 -0600

    run prettier

--- packages/starter/example/src/pages/_app.tsx
+++ packages/starter/example/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import type { FC } from "react";
-import React from "react";
-import { ContextProvider } from "../components/ContextProvider";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import type { FC } from 'react';
+import React from 'react';
+import { ContextProvider } from '../components/ContextProvider';
 
 // Use require instead of import since order matters
 require('antd/dist/antd.dark.less');
--- packages/starter/nextjs-starter/src/pages/_app.tsx
+++ packages/starter/nextjs-starter/src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import type { AppProps } from "next/app";
-import type { FC } from "react";
-import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import type { AppProps } from 'next/app';
+import type { FC } from 'react';
+import React, { useMemo } from 'react';
 
 // Use require instead of import since order matters
 require('@solana/wallet-adapter-react-ui/styles.css');
