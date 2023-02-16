b2ecfcc164adbb9556f1155a5a76752e8148366f
Jordan Sexton <jordan@jordansexton.com>
2023-02-16 17:30:14 -0600

    move transaction types

--- packages/core/base/src/index.ts
+++ packages/core/base/src/index.ts
@@ -2,4 +2,5 @@ export * from './adapter.js';
 export * from './errors.js';
 export * from './signer.js';
 export * from './standard.js';
+export * from './transaction.js';
 export * from './types.js';
--- packages/core/base/src/types.ts
+++ packages/core/base/src/types.ts
@@ -1,4 +1,3 @@
-import type { Transaction, TransactionVersion, VersionedTransaction } from '@solana/web3.js';
 import type { WalletAdapter } from './adapter.js';
 import type { MessageSignerWalletAdapter, SignerWalletAdapter } from './signer.js';
 import type { StandardWalletAdapter } from './standard.js';
@@ -9,16 +8,4 @@ export enum WalletAdapterNetwork {
     Mainnet = 'mainnet-beta',
     Testnet = 'testnet',
     Devnet = 'devnet',
-}
-
-export type SupportedTransactionVersions = ReadonlySet<TransactionVersion> | null | undefined;
-
-export type TransactionOrVersionedTransaction<S extends SupportedTransactionVersions> = S extends null | undefined
-    ? Transaction
-    : Transaction | VersionedTransaction;
-
-export function isVersionedTransaction(
-    transaction: Transaction | VersionedTransaction
-): transaction is VersionedTransaction {
-    return 'version' in transaction;
 }
