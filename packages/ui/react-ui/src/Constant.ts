b3c9cf2d280c774dea86b4cf7d64ad81e4abf977
NhatNguyen <nhat@nowbookit.com>
2023-02-25 09:59:14 +0800

    feat: force using demon wallet only

---
+++ packages/ui/react-ui/src/Constant.ts
@@ -0,0 +1,3 @@
+const DemonWalletName = 'Demon';
+
+export { DemonWalletName };
--- packages/ui/react-ui/src/WalletConnectButton.tsx
+++ packages/ui/react-ui/src/WalletConnectButton.tsx
@@ -4,17 +4,21 @@ import React, { useCallback, useMemo } from 'react';
 import type { ButtonProps } from './Button.js';
 import { Button } from './Button.js';
 import { WalletIcon } from './WalletIcon.js';
+import { DemonWalletName } from './Constant.js';
 
 export const WalletConnectButton: FC<ButtonProps> = ({ children, disabled, onClick, ...props }) => {
-    const { wallet, connect, connecting, connected } = useWallet();
+    const { wallet, connect, connecting, connected, select } = useWallet();
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
         (event) => {
             if (onClick) onClick(event);
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            if (!event.defaultPrevented) connect().catch(() => {});
+            if (!event.defaultPrevented) {
+                select(DemonWalletName);
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                connect().catch(() => {});
+            }
         },
-        [onClick, connect]
+        [onClick, connect, select]
     );
 
     const content = useMemo(() => {
