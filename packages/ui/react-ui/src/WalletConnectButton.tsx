25e32cf713f1920b963f2731b470686dd061429d
Steven Luscher <steven.luscher@solana.com>
2023-06-12 23:10:41 +0000

    feat: implement the React UI starter UI atop the new wallet UI hooks ## Summary
    This PR proves out the `useWalletConnectButton`, `useWalletDisconnectButton` and `useWalletMultiButton` hooks by re-implementing the React UI UI package atop it. The package no longer uses `useWallet`, and has less connection-wrangling logic. This establishes a pattern that other developers can use to ship their own custom wallet connection UI.
    
    ## Test Plan
    
    Loaded the example app and all three starter apps. Buttons work as they did before.
    
    Finishes #658.

--- packages/ui/react-ui/src/WalletConnectButton.tsx
+++ packages/ui/react-ui/src/WalletConnectButton.tsx
@@ -1,39 +1,41 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import type { FC, MouseEventHandler } from 'react';
-import React, { useCallback, useMemo } from 'react';
+import { useWalletConnectButton } from '@solana/wallet-adapter-react';
+import React from 'react';
+import { BaseWalletConnectionButton } from './BaseWalletConnectionButton.js';
 import type { ButtonProps } from './Button.js';
-import { Button } from './Button.js';
-import { WalletIcon } from './WalletIcon.js';
 
-export const WalletConnectButton: FC<ButtonProps> = ({ children, disabled, onClick, ...props }) => {
-    const { wallet, connect, connecting, connected } = useWallet();
-
-    const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
-        (event) => {
-            if (onClick) onClick(event);
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            if (!event.defaultPrevented) connect().catch(() => {});
-        },
-        [onClick, connect]
-    );
-
-    const content = useMemo(() => {
-        if (children) return children;
-        if (connecting) return 'Connecting ...';
-        if (connected) return 'Connected';
-        if (wallet) return 'Connect';
-        return 'Connect Wallet';
-    }, [children, connecting, connected, wallet]);
-
+export function WalletConnectButton({ children, disabled, onClick, ...props }: ButtonProps) {
+    const { buttonDisabled, buttonState, onButtonClick, walletIcon, walletName } = useWalletConnectButton();
+    let label;
+    if (children) {
+        label = children;
+    } else if (buttonState === 'connecting') {
+        label = 'Connecting ...';
+    } else if (buttonState === 'connected') {
+        label = 'Connected';
+    } else if (buttonState === 'has-wallet') {
+        label = 'Connect';
+    } else {
+        label = 'Connect Wallet';
+    }
     return (
-        <Button
-            className="wallet-adapter-button-trigger"
-            disabled={disabled || !wallet || connecting || connected}
-            startIcon={wallet ? <WalletIcon wallet={wallet} /> : undefined}
-            onClick={handleClick}
+        <BaseWalletConnectionButton
             {...props}
+            disabled={disabled || buttonDisabled}
+            onClick={(e) => {
+                if (onClick) {
+                    onClick(e);
+                }
+                if (e.defaultPrevented) {
+                    return;
+                }
+                if (onButtonClick) {
+                    onButtonClick();
+                }
+            }}
+            walletIcon={walletIcon}
+            walletName={walletName}
         >
-            {content}
-        </Button>
+            {label}
+        </BaseWalletConnectionButton>
     );
-};
+}
--- packages/ui/react-ui/src/WalletMultiButton.tsx
+++ packages/ui/react-ui/src/WalletMultiButton.tsx
@@ -1,48 +1,19 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import type { FC } from 'react';
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useWalletMultiButton } from '@solana/wallet-adapter-react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { BaseWalletConnectionButton } from './BaseWalletConnectionButton.js';
 import type { ButtonProps } from './Button.js';
-import { Button } from './Button.js';
 import { useWalletModal } from './useWalletModal.js';
-import { WalletConnectButton } from './WalletConnectButton.js';
-import { WalletIcon } from './WalletIcon.js';
-import { WalletModalButton } from './WalletModalButton.js';
 
-export const WalletMultiButton: FC<ButtonProps> = ({ children, ...props }) => {
-    const { publicKey, wallet, disconnect } = useWallet();
-    const { setVisible } = useWalletModal();
+export function WalletMultiButton({ children, ...props }: ButtonProps) {
+    const { setVisible: setModalVisible } = useWalletModal();
+    const { buttonState, onConnect, onDisconnect, publicKey, walletIcon, walletName } = useWalletMultiButton({
+        onSelectWallet() {
+            setModalVisible(true);
+        },
+    });
     const [copied, setCopied] = useState(false);
-    const [active, setActive] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     const ref = useRef<HTMLUListElement>(null);
-
-    const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
-    const content = useMemo(() => {
-        if (children) return children;
-        if (!wallet || !base58) return null;
-        return base58.slice(0, 4) + '..' + base58.slice(-4);
-    }, [children, wallet, base58]);
-
-    const copyAddress = useCallback(async () => {
-        if (base58) {
-            await navigator.clipboard.writeText(base58);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 400);
-        }
-    }, [base58]);
-
-    const openDropdown = useCallback(() => {
-        setActive(true);
-    }, []);
-
-    const closeDropdown = useCallback(() => {
-        setActive(false);
-    }, []);
-
-    const openModal = useCallback(() => {
-        setVisible(true);
-        closeDropdown();
-    }, [setVisible, closeDropdown]);
-
     useEffect(() => {
         const listener = (event: MouseEvent | TouchEvent) => {
             const node = ref.current;
@@ -50,7 +21,7 @@ export const WalletMultiButton: FC<ButtonProps> = ({ children, ...props }) => {
             // Do nothing if clicking dropdown or its descendants
             if (!node || node.contains(event.target as Node)) return;
 
-            closeDropdown();
+            setMenuOpen(false);
         };
 
         document.addEventListener('mousedown', listener);
@@ -60,39 +31,89 @@ export const WalletMultiButton: FC<ButtonProps> = ({ children, ...props }) => {
             document.removeEventListener('mousedown', listener);
             document.removeEventListener('touchstart', listener);
         };
-    }, [ref, closeDropdown]);
-
-    if (!wallet) return <WalletModalButton {...props}>{children}</WalletModalButton>;
-    if (!base58) return <WalletConnectButton {...props}>{children}</WalletConnectButton>;
-
+    }, []);
+    const content = useMemo(() => {
+        if (children) {
+            return children;
+        } else if (buttonState === 'connecting') {
+            return 'Connecting ...';
+        } else if (publicKey) {
+            const base58 = publicKey.toBase58();
+            return base58.slice(0, 4) + '..' + base58.slice(-4);
+        } else if (buttonState === 'has-wallet') {
+            return 'Connect';
+        } else {
+            return 'Select Wallet';
+        }
+    }, [buttonState, children, publicKey]);
     return (
         <div className="wallet-adapter-dropdown">
-            <Button
-                aria-expanded={active}
-                className="wallet-adapter-button-trigger"
-                style={{ pointerEvents: active ? 'none' : 'auto', ...props.style }}
-                onClick={openDropdown}
-                startIcon={<WalletIcon wallet={wallet} />}
+            <BaseWalletConnectionButton
                 {...props}
+                aria-expanded={menuOpen}
+                style={{ pointerEvents: menuOpen ? 'none' : 'auto', ...props.style }}
+                onClick={() => {
+                    switch (buttonState) {
+                        case 'no-wallet':
+                            setModalVisible(true);
+                            break;
+                        case 'has-wallet':
+                            if (onConnect) {
+                                onConnect();
+                            }
+                            break;
+                        case 'connected':
+                            setMenuOpen(true);
+                            break;
+                    }
+                }}
+                walletIcon={walletIcon}
+                walletName={walletName}
             >
                 {content}
-            </Button>
+            </BaseWalletConnectionButton>
             <ul
                 aria-label="dropdown-list"
-                className={`wallet-adapter-dropdown-list ${active && 'wallet-adapter-dropdown-list-active'}`}
+                className={`wallet-adapter-dropdown-list ${menuOpen && 'wallet-adapter-dropdown-list-active'}`}
                 ref={ref}
                 role="menu"
             >
-                <li onClick={copyAddress} className="wallet-adapter-dropdown-list-item" role="menuitem">
-                    {copied ? 'Copied' : 'Copy address'}
-                </li>
-                <li onClick={openModal} className="wallet-adapter-dropdown-list-item" role="menuitem">
+                {publicKey ? (
+                    <li
+                        className="wallet-adapter-dropdown-list-item"
+                        onClick={async () => {
+                            await navigator.clipboard.writeText(publicKey.toBase58());
+                            setCopied(true);
+                            setTimeout(() => setCopied(false), 400);
+                        }}
+                        role="menuitem"
+                    >
+                        {copied ? 'Copied' : 'Copy address'}
+                    </li>
+                ) : null}
+                <li
+                    className="wallet-adapter-dropdown-list-item"
+                    onClick={() => {
+                        setModalVisible(true);
+                        setMenuOpen(false);
+                    }}
+                    role="menuitem"
+                >
                     Change wallet
                 </li>
-                <li onClick={disconnect} className="wallet-adapter-dropdown-list-item" role="menuitem">
-                    Disconnect
-                </li>
+                {onDisconnect ? (
+                    <li
+                        className="wallet-adapter-dropdown-list-item"
+                        onClick={() => {
+                            onDisconnect();
+                            setMenuOpen(false);
+                        }}
+                        role="menuitem"
+                    >
+                        Disconnect
+                    </li>
+                ) : null}
             </ul>
         </div>
     );
-};
+}
