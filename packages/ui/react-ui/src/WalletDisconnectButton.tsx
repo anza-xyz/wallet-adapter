4fe38cf19856648bd68b1f4e0ebbc50994316840
Steven Luscher <steveluscher@users.noreply.github.com>
2023-07-05 09:48:47 -0700

    feat: implement the React UI starter UI atop the new wallet UI hooks (#792)
    * feat: implement the React UI starter UI atop the new wallet UI hooks
    ## Summary
    
    This PR proves out the `useWalletConnectButton`, `useWalletDisconnectButton` and `useWalletMultiButton` hooks by re-implementing the React UI UI package atop it. The package no longer uses `useWallet`, and has less connection-wrangling logic. This establishes a pattern that other developers can use to ship their own custom wallet connection UI.
    
    ## Test Plan
    
    Loaded the example app and all three starter apps. Buttons work as they did before.
    
    Finishes #658.

--- packages/ui/react-ui/src/WalletDisconnectButton.tsx
+++ packages/ui/react-ui/src/WalletDisconnectButton.tsx
@@ -1,38 +1,39 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import type { FC, MouseEventHandler } from 'react';
-import React, { useCallback, useMemo } from 'react';
+import { useWalletDisconnectButton } from '@solana/wallet-adapter-react';
+import React from 'react';
+import { BaseWalletConnectionButton } from './BaseWalletConnectionButton.js';
 import type { ButtonProps } from './Button.js';
-import { Button } from './Button.js';
-import { WalletIcon } from './WalletIcon.js';
 
-export const WalletDisconnectButton: FC<ButtonProps> = ({ children, disabled, onClick, ...props }) => {
-    const { wallet, disconnect, disconnecting } = useWallet();
-
-    const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
-        (event) => {
-            if (onClick) onClick(event);
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            if (!event.defaultPrevented) disconnect().catch(() => {});
-        },
-        [onClick, disconnect]
-    );
-
-    const content = useMemo(() => {
-        if (children) return children;
-        if (disconnecting) return 'Disconnecting ...';
-        if (wallet) return 'Disconnect';
-        return 'Disconnect Wallet';
-    }, [children, disconnecting, wallet]);
-
+export function WalletDisconnectButton({ children, disabled, onClick, ...props }: ButtonProps) {
+    const { buttonDisabled, buttonState, onButtonClick, walletIcon, walletName } = useWalletDisconnectButton();
+    let label;
+    if (children) {
+        label = children;
+    } else if (buttonState === 'disconnecting') {
+        label = 'Disconnecting ...';
+    } else if (buttonState === 'has-wallet') {
+        label = 'Disconnect';
+    } else {
+        label = 'Disconnect Wallet';
+    }
     return (
-        <Button
-            className="wallet-adapter-button-trigger"
-            disabled={disabled || !wallet}
-            startIcon={wallet ? <WalletIcon wallet={wallet} /> : undefined}
-            onClick={handleClick}
+        <BaseWalletConnectionButton
             {...props}
+            disabled={disabled || buttonDisabled}
+            onClick={(e) => {
+                if (onClick) {
+                    onClick(e);
+                }
+                if (e.defaultPrevented) {
+                    return;
+                }
+                if (onButtonClick) {
+                    onButtonClick();
+                }
+            }}
+            walletIcon={walletIcon}
+            walletName={walletName}
         >
-            {content}
-        </Button>
+            {label}
+        </BaseWalletConnectionButton>
     );
-};
+}
--- packages/ui/react-ui/src/WalletModalButton.tsx
+++ packages/ui/react-ui/src/WalletModalButton.tsx
@@ -1,7 +1,7 @@
 import type { FC, MouseEvent } from 'react';
 import React, { useCallback } from 'react';
 import type { ButtonProps } from './Button.js';
-import { Button } from './Button.js';
+import { Button as BaseWalletConnectionButton } from './Button.js';
 import { useWalletModal } from './useWalletModal.js';
 
 export const WalletModalButton: FC<ButtonProps> = ({ children = 'Select Wallet', onClick, ...props }) => {
@@ -16,8 +16,8 @@ export const WalletModalButton: FC<ButtonProps> = ({ children = 'Select Wallet',
     );
 
     return (
-        <Button className="wallet-adapter-button-trigger" onClick={handleClick} {...props}>
+        <BaseWalletConnectionButton {...props} className="wallet-adapter-button-trigger" onClick={handleClick}>
             {children}
-        </Button>
+        </BaseWalletConnectionButton>
     );
 };
