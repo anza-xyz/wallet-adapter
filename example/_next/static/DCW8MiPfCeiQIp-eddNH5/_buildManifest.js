5482158786ad2f80417ac5b81a1435f9810626aa
Jordan Sexton <jordan@jordansexton.com>
2023-07-20 16:43:27 -0600

    Updates

---
+++ example/_next/static/DCW8MiPfCeiQIp-eddNH5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/484-3cb8b9a05933e803.js","static/chunks/pages/index-516bce7b41b58e82.js"],"/_error":["static/chunks/pages/_error-cb20cc17d24562c1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ example/_next/static/chunks/pages/_error-cb20cc17d24562c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{3365:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(4837)}])}},function(n){n.O(0,[774,888,179],(function(){return _=3365,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
