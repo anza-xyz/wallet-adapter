a94451b380c5ef085239d8db374c8d6653f10611
Varga-Somogyi Ákos <vsakos@gmail.com>
2023-09-05 18:31:07 +0200

    Export initializeSolflareMetaMask

--- packages/wallets/solflare/src/index.ts
+++ packages/wallets/solflare/src/index.ts
@@ -1,1 +1,5 @@
 export * from './adapter.js';
+export {
+    initialize as initializeSolflareMetaMask,
+    initializeWhenDetected as initializeSolflareMetaMaskWhenDetected,
+} from './metamask/initialize.js';
