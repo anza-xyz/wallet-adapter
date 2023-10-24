fd34bbb49bd06072cc649526442d7d5358a0fb36
Jordan Sexton <jordan@jordansexton.com>
2023-10-24 17:01:48 -0600

    fix method name

--- packages/core/base/src/signer.ts
+++ packages/core/base/src/signer.ts
@@ -24,7 +24,7 @@ export abstract class BaseSignerWalletAdapter<Name extends string = string>
     extends BaseWalletAdapter<Name>
     implements SignerWalletAdapter<Name>
 {
-    async sendTransaction(
+    async signAndSendTransaction(
         transaction: TransactionOrVersionedTransaction<this['supportedTransactionVersions']>,
         connection: Connection,
         options: SignAndSendTransactionOptions = {}
