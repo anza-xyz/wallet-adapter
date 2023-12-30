cb9401b8c560ccf8a79e3a296e5dc83d81141e55
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-12-30 16:22:21 +0530

    feat: Update Nextjs Starter to v14 using the App router

---
+++ packages/starter/nextjs-starter/src/components/wallet-multi-button.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const WalletDisconnectButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletDisconnectButton,
+  { ssr: false }
+);
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+export { WalletDisconnectButtonDynamic, WalletMultiButtonDynamic };
