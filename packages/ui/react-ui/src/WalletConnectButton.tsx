f78d5c65cdf617817b9996726dbe12dbe1e0755b
techiejd <62455039+techiejd@users.noreply.github.com>
2024-05-28 23:07:50 +0700

    Fixes build error from similar named props export.
    Build error: Module './BaseWalletConnectButton.js' has already exported
    a member named 'Props'. Consider explicitly re-exporting to resolve the
    ambiguity.

--- packages/ui/react-ui/src/WalletConnectButton.tsx
+++ packages/ui/react-ui/src/WalletConnectButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BaseWalletConnectButton, Props as BaseProps } from './BaseWalletConnectButton.js';
 
-export type Props = Omit<BaseProps, 'labels'> & {labels?: Partial<BaseProps['labels']>};
+export type WalletConnectButtonProps = Omit<BaseProps, 'labels'> & {labels?: Partial<BaseProps['labels']>};
 
 const LABELS = {
     connecting: 'Connecting ...',
@@ -10,7 +10,7 @@ const LABELS = {
     'no-wallet': 'Connect Wallet',
 } as const;
 
-export function WalletConnectButton(props: Props) {
+export function WalletConnectButton(props: WalletConnectButtonProps) {
     props.labels = { ...LABELS, ...props.labels };
     return <BaseWalletConnectButton {...(props as BaseProps)} />;
 }
